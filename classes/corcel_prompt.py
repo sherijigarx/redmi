98310107619c4adb52acd3d37cbe78caedb53a3f
isharab <155580193+sherijigarx@users.noreply.github.com>
2024-03-25 22:05:24 +0500

    Update corcel_prompt.py

--- classes/corcel_prompt.py
+++ classes/corcel_prompt.py
@@ -5,8 +5,10 @@ import os
 class CorcelAPI:
     def __init__(self):
         self.base_url = "https://api.corcel.io/v1/text/cortext/chat"
-        self.api_key = os.getenv('API_KEY')
+        self.api_key = os.environ.get['CORCEL_API_KEY']
         if self.api_key is None:
+            bt.logging.info(f"Corcel API is: {self.api_key}")
+            bt.logging.error("Corcel API key not found.")
             pass
         self.headers = {
             "Authorization": self.api_key,
